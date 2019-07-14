333f11162d524dd6a64c36c6c95c8e2ca93631c2
王承权 <wangchengquan@qiyi.com>
2019-07-14 16:33:32 +0800

    update

--- Example/WKRefresh/WKViewController.m
+++ Example/WKRefresh/WKViewController.m
@@ -10,6 +10,10 @@
 
 @interface WKViewController ()
 
+@property (nonatomic, strong) UIImageView *loadingView;
+
+@property (nonatomic, assign) BOOL isPlaying;
+
 @end
 
 @implementation WKViewController
@@ -17,7 +21,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+      self.loadingView = [[UIImageView alloc]initWithFrame:CGRectMake(150, 300, 50, 50)];
+        self.loadingView.image = [UIImage imageNamed:@"con_ic_ending"];
+        [self.view addSubview:self.loadingView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self play];
+    self.isPlaying = YES;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (self.isPlaying) {
+        [self stop];
+    } else {
+        [self play];
+    }
+    self.isPlaying = !self.isPlaying;
+}
+
+- (void)play {
+    CABasicAnimation* rotationAnimation;
+        rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+        rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 0.5 ];
+        rotationAnimation.duration = 0.2;
+        rotationAnimation.cumulative = YES;
+        rotationAnimation.repeatCount = MAXFLOAT;
+        [self.loadingView.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];//开始动画
+}
+
+- (void)stop {
+    [self.loadingView.layer removeAnimationForKey:@"rotationAnimation"];
 }
 
 - (void)didReceiveMemoryWarning
